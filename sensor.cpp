73999f452b644d86372273873389a252778b1771
flora <hanbing.lin@stonybrook.edu>
2017-02-17 16:05:00 -0500

    stop&resume

--- sensor.cpp
+++ sensor.cpp
@@ -3,6 +3,20 @@
 
 void sensor::dosens(){
 
+	if (in_stop == 0){
+		if (obstacle <= range){
+			stop_ = 1;
+			out_stop = stop_;
+		}
+	}
+	else {
+		if (obstacle >= range){
+			res_ = 1;
+			res = res_;
+		}
+	}
 
-		if 
+
 }
+
+	
